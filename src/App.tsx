d8009a9040f61c1b190d1bd01af0bc4448414cc8
Sander Wegdam <sanderwegdam@gmail.com>
2022-06-25 16:37:09 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Winkel } from "./pages/Winkel"
+import { Home } from "./pages/Home"
 import { Navbar } from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 
@@ -10,7 +11,8 @@ function App() {
       <Navbar />
       <Container className="mb-4">
         <Routes>   
-          <Route path="/" element={<Winkel />} />
+          <Route path="/winkel" element={<Winkel />} />
+           <Route path="/" element={<Home />} />
           <Route path="/*" element={<Navigate to="/" />}  />
         </Routes>
       </Container>
