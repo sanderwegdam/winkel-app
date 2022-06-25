fa2d2ebcc5d837074b576768212465e531c451b2
Sander Wegdam <sanderwegdam@gmail.com>
2022-06-25 14:10:47 +0200

    update

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -13,7 +13,6 @@ export function CartItem({ id }: CartItemProps) {
     getItemQuantity,
     increaseCartQuantity,
     decreaseCartQuantity,
-    removeFromCart,
   } = useShoppingCart()
   const quantity = getItemQuantity(id)
 
